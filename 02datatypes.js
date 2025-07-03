0cf343f034aab9f2e96b35afd122692935e4b9fe
ParasMahla <parasmahla444@gmail.com>
2025-07-03 10:09:40 +0530

    dataTypes

--- 02datatypes.js
+++ 02datatypes.js
@@ -1,2 +1,15 @@
 //"use strict" //treat all js code as newer version
 // alert (3 + 3) // we are using node.js, not browser
+// null => standalone value
+// undefined =>
+//symbol => unique
+//object
+// today's assignment go and see the specification on the ecma website.
+
+console.log(typeof undefined); //undefined
+console.log(typeof null);  // Object
+
+let name = "paras"; //string => ""
+let age = 22;  //number => 2 to power 53
+let isloggedIn = false;  // boolean => true flase
+console.log( isloggedIn);
