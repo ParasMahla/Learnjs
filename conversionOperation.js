bfcea8678ec61a02e1c9c556d9d33ec81758a5aa
ParasMahla <parasmahla444@gmail.com>
2025-07-15 11:09:32 +0530

    new changes

--- conversionOperation.js
+++ conversionOperation.js
@@ -5,4 +5,21 @@ console.log(negValue);
 // console.log(2+2);
 // console.log(2-2);
 // console.log(2*2);
+//console.log(2**2) double star is used for denoting the power
 // console.log(2/2);
+//console.log(2%2);
+
+
+let str1 = "Paras";
+let str2 = "Hello";
+
+let str3 = str2+str1;
+console.log(str3);
+
+console.log("1" + 2);   // The output is 12
+console.log(1 + "2");  //The output is 12
+console.log("1" + 2 + 3);  //the output will be 123
+console.log(1 + 2 + "3");  //The output is 33 : Because if the string comes in the starting then the entire numbers next to it behaves like string and if the number comes first then the operation are performed and then added to the string.
+
+let num1, num2, num3
+ num1 = num2 = num3 = 2 + 2
