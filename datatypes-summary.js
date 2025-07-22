83e14d3ad5dc8e4eb24e10186b44767b7cfe5049
ParasMahla <parasmahla444@gmail.com>
2025-07-22 23:57:32 +0530

    datatypesSummary

---
+++ datatypes-summary.js
@@ -0,0 +1,21 @@
+//    Primitive
+
+//    7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100;
+const scoreValue  = 100.3;
+
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail;
+
+const anotherId = Symbol('123')
+
+const bigNumber = 986326342639834324242422242n
+
+
+
+// Refrence (non-premitive)
+
+// Array, Objects, Functions
+
